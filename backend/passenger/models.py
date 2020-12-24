ec633c753082c472507d9ca620bafa8883302ded
Haridev <haridevbabu@zoho.com>
2020-12-24 23:18:41 +0530

    Created models for storing driver and passenger details

--- backend/passenger/models.py
+++ backend/passenger/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Store passenger details
+"""
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+    mobile = models.IntegerField(unique=True)
+
+class Driver(models.Model):
+    """
+    Storing driver details
+    """
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    mobile = models.IntegerField(unique=True)
+    car_no = models.CharField(max_length=80, unique=True)
